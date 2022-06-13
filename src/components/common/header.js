36322dca25946c93b4aede48a5ab67998639292b
Jorge P <hi@jorgepardo.dev>
2022-06-13 09:36:12 +0200

    Added header, footer and some image placeholders

---
+++ src/components/common/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Personal = () => {
+    return (  
+        <div>
+            <header className="App-header">
+                <p data-aos="fade-in">
+                    Aquí irá algo mas épico...
+                </p>
+            </header>
+        </div>
+    );
+};
