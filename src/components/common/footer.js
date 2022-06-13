5cd78de7ed4f485a7aac37669d0ea804b7ffe4af
Jorge P <hi@jorgepardo.dev>
2022-06-13 10:17:32 +0200

    Adding a dark mode switch

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Personal = () => {
+export const Footer = () => {
     return (  
         <div>
-            <header className="App-header">
+            <header className="h-10">
                 <p data-aos="fade-in">
                     Aquí irá algo mas épico...
                 </p>
