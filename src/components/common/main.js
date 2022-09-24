f93908713169ac48f335996de8dc86af980c5239
Jorge Pardo <hi@jorgepardo.dev>
2022-09-24 19:04:33 +0200

    El carrusel del home funciona, le falta css a las imagenes

--- src/components/common/main.js
+++ src/components/common/main.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Slider from "react-slick";
 
+
 export const Main = () => {
   const mainCarousel = {
     dots: false,
+    fade: true,
     infinite: true,
     speed: 1500,
     autoplaySpeed: 3000,
@@ -16,13 +18,13 @@ export const Main = () => {
 
   return (
     <section className="bg-DarkBG">
-      <header className="absolute px-4 py-32 mx-auto max-w-screen-xl lg:h-screen lg:items-center lg:flex">
+      <header className="absolute z-20 px-4 pl-40 max-w-screen-xl lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl text-center sm:text-left">
           <h1 className="text-5xl text-DarkGR font-Ral text-center sm:text-5xl">
             JORGEPARDO.DEV
           </h1>
 
-          <nav className="flex flex-wrap mt-8 text-center gap-4 cl-effect-18">
+          <nav className="flex flex-wrap mt-8 text-center gap-1 cl-effect-18">
             <a
               href="/graphicdesign"
               className="font-Ral text-xl"
@@ -36,136 +38,47 @@ export const Main = () => {
             <a href="/graphicdesign" className="font-Ral" data-hover="Graphic">
               Profile
             </a>
+            <a href="/graphicdesign" className="font-Ral" data-hover="Graphic">
+              Photo
+            </a>
           </nav>
         </div>
       </header>
-{/*  */}
-
-      {/* <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-TW-bg-dark sm:to-transparent"></div> */}
-      {/* <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-TW-bg-dark sm:to-transparent"></div> */}
-      {/* <img
-        className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
-        src="https://res.cloudinary.com/jorgepardor/image/upload/v1663358017/jorgepardo.dev/hero/2347341057_bb3d81cb7e_o_jhifmr.webp"
-        alt="..."
-      /> */}
 
 
-{/* 
-      <div
-        id="default-carousel"
-        className="relative z-0 inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100"
-        data-carousel="slide"
-      >
-        <div className=" h-56 overflow-hidden rounded-lg md:h-96">
-          <div
-            className=" duration-75 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
-            data-carousel-item=""
-          >
-            <span className="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">
-              First Slide
-            </span>
-            <img
-              src="https://res.cloudinary.com/jorgepardor/image/upload/v1663358017/jorgepardo.dev/hero/2347341057_bb3d81cb7e_o_jhifmr.webp"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-
-          <div
-            className="duration-100 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10"
-            data-carousel-item=""
-          >
-            <img
-              src="https://res.cloudinary.com/jorgepardor/image/upload/v1663358017/jorgepardo.dev/hero/2347341057_bb3d81cb7e_o_jhifmr.webp"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-
-          <div
-            className="duration-100 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10"
-            data-carousel-item=""
-          >
-            <img
-              src="https://res.cloudinary.com/jorgepardor/image/upload/v1663358017/jorgepardo.dev/hero/2347341057_bb3d81cb7e_o_jhifmr.webpg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-        </div>
-        <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-white dark:bg-gray-800"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
-        </div>
-        n
-        <button
-          type="button"
-          className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev=""
-        >
-          <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 19l-7-7 7-7"
-              ></path>
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next=""
-        >
-          <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 5l7 7-7 7"
-              ></path>
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </div> */}
+      <div className="hidden z-10 sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-TW-bg-dark sm:to-transparent"></div>
+      <div className="hidden z-10 sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-TW-bg-dark sm:to-transparent"></div>
+   
+      <Slider {...mainCarousel}>
+            <div className="w-100 h-screen mx-auto">
+              <img
+                className="mx-auto"
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1663358017/jorgepardo.dev/hero/2347341057_bb3d81cb7e_o_jhifmr.webp"
+                alt="Imagen de variedades"
+              />
+            </div>
+            <div className="w-100 h-screen mx-auto">
+              <img
+                className="mx-auto"
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1663357960/jorgepardo.dev/hero/5106809456_cc3d6fa787_o_wbfpmf.jpg"
+                alt="Imagen de variedades"
+              />
+            </div>
+            <div className="w-100 h-screen mx-auto">
+              <img
+                className="mx-auto"
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1663357961/jorgepardo.dev/hero/4535947260_32b748bfdd_o_cepuxm.jpg"
+                alt="Imagen de variedades"
+              />
+            </div>
+            <div className="w-100 h-screen mx-auto">
+              <img
+                className="..."
+                src="https://res.cloudinary.com/jorgepardor/image/upload/v1663357960/jorgepardo.dev/hero/2937250416_317367315e_o_jadhzj.jpg"
+                alt="Imagen de variedades"
+              />
+            </div>
+          </Slider>
     </section>
   );
 };
