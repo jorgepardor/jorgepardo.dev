60eb940ae294acfaf66ffb22f7e99674a59b183e
Jorge Pardo <hi@jorgepardo.dev>
2022-10-29 09:36:07 +0200

    Creada la pagina de enlaces

---
+++ src/components/common/links.js
@@ -0,0 +1,14 @@
+import React from "react";
+export const Links = () => {
+  return (
+    <div className=" bg-TW-bg-dark">
+      <button
+        type="button"
+        class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+      >
+
+        LinkedIn
+      </button>
+    </div>
+  );
+};
---
+++ src/pages/links-landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Links } from "../components/common/links";
+
+export const LinksLanding = () => {
+  return (
+    <div className=" bg-TW-bg-dark">
+      <Links />
+    </div>
+  );
+};
